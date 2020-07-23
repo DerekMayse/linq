604db7fbeccb115bb0aa84448bc294d852ce813f
Your Name <you@example.com>
2020-07-23 08:30:00 -0400

    new repository

---
+++ Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Restriction/Filtering Operations
+
+            // Find the words in the collection that start with the letter 'L'
+            char[] startingLetters = new char[] { 'L' };
+            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+
+
+
+            List<string> LFruits = (from fruit in fruits where fruit.IndexOfAny(startingLetters, 0, 1) == 0 select fruit).ToList();
+
+            // LFruits.ForEach(LFruit => Console.WriteLine(LFruit));
+
+
+            // Which of the following numbers are multiples of 4 or 6
+            //     List<int> numbers = new List<int>()
+            //    {
+            //     15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            //    };
+
+            //     List<int> fourSixMultiples = numbers.Where(number => number % 4 == 0 && number % 6 == 0).ToList();
+
+            // fourSixMultiples.ForEach(fourSixMultiple => Console.WriteLine(fourSixMultiple));
+
+            // Ordering Operations
+            // Order these student names alphabetically, in descending order (Z to A)
+            List<string> names = new List<string>()
+{
+    "Heather", "James", "Xavier", "Michelle", "Brian", "Nina",
+    "Kathleen", "Sophia", "Amir", "Douglas", "Zarley", "Beatrice",
+    "Theodora", "William", "Svetlana", "Charisse", "Yolanda",
+    "Gregorio", "Jean-Paul", "Evangelina", "Viktor", "Jacqueline",
+    "Francisco", "Tre"
+};
+
+            List<string> descend = names.OrderByDescending(name => name).ToList();
+
+            // descend.ForEach(descendName => Console.WriteLine(descendName));
+
+            // Build a collection of these numbers sorted in ascending order
+            // List<int> numbers = new List<int>()
+            // {
+            //     15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            // };
+
+            // List<int> ascendingNums = numbers.OrderBy(number => number).ToList();
+
+            // ascendingNums.ForEach(ascendingNum => Console.WriteLine(ascendingNum));
+
+            // Output how many numbers are in this list
+            // List<int> numbers = new List<int>()
+            // {
+            //     15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            // };
+
+            // int numOfNumbers = numbers.Count();
+
+            // Console.WriteLine(numOfNumbers);
+
+            // How much money have we made?
+            // List<double> purchases = new List<double>()
+            // {
+            //     2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
+            // };
+
+            // double totalMoney = purchases.Sum();
+
+            // Console.WriteLine(totalMoney);
+
+            // What is our most expensive product?
+            // List<double> prices = new List<double>()
+            // {
+            //     879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
+            // };
+
+            // double expensive = prices.Max();
+
+            // Console.WriteLine(expensive);
+
+            // Partitioning Operations
+
+            // List<int> wheresSquaredo = new List<int>()
+            // {
+            //     66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
+            // };
+
+
+
+            // List<int> foundSquaredo = wheresSquaredo.TakeWhile(square =>  Math.Sqrt(square)% 1 !=0).ToList();
+
+
+
+
+            // Console.WriteLine();
+
+            // dont need foreach use different kind of loop
+            // math.sqrt()% 1 ==0
+            // need to return something in found squaredo
+            // 1 to 2 steps to make that happen
+            // take everything until the first perfect square
+
+
+
+            // Store each number in the following List until a perfect square
+            // is detected.
+
+            // Expected output is { 66, 12, 8, 27, 82, 34, 7, 50, 19, 46 } 
+
+
+
+
+            List<Customer> customers = new List<Customer>() {
+            new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+            new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+            new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+            new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+            new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+            new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+            new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+            new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+            new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+            new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+
+
+
+        };
+
+        List<Customer> millionaires = customers.Where(customer => customer.Balance > 1000000).ToList();
+
+                    var banksMillionaires = millionaires.GroupBy(
+       customer=> customer.Bank,
+     
+      (key, customerList) => new { Bank = key, CustomerCount = customerList.ToList().Count()});
+
+
+        Console.WriteLine();
+
+
+        }
+    }
+}
